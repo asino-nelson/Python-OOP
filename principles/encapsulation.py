f70b5c37ca71650db33b14c62603c70b74c3b5b9
asino-nelson <asinonelson276@gmail.com>
2025-02-01 10:24:30 +0300

    encapsulation

---
+++ principles/encapsulation.py
@@ -0,0 +1,36 @@
+# Encapsulation
+
+class Bad_bank_account:
+    def __init__(self,balance):
+        self._balance = balance
+
+
+
+class Bank_account:
+    def __init__(self,balance = 0.0):
+        self._balance = balance
+
+    @property
+    def balance(self):
+        return self._balance
+    
+    # Deposit
+    def deposit(self, amount):
+        if amount<=0:
+            raise ValueError("Amount deposited must be greater than 0")
+        self._balance += amount
+        
+    # Withdraw
+    def withdraw(self, amount):
+        if amount<=0:
+            raise ValueError("Failed! Amount withdrawn must be greater than 0")
+        elif amount > self._balance:
+            raise ValueError("Failed! You have insuffient funds")
+        self._balance -= amount
+
+account = Bank_account()
+print(account.balance)
+account.deposit(200)
+print(account.balance)
+account.withdraw(150)
+print(account.balance)
