1c93aa0263cb844b675699f9516d960689611c0e
asino-nelson <asinonelson276@gmail.com>
2025-02-16 18:08:56 +0300

    Quadratic time O(n²)

---
+++ DSA/BigO/Quadratic.py
@@ -0,0 +1,27 @@
+# O(n²)
+
+n = [0,1,2,3,4]
+for i in n:
+    i += 1
+    print(f"i = {i}")
+    print("")
+    for j in n:
+        j += 1
+        print(f"j = {j}")
+    print("")
+
+
+    
+
+print("Example 2")
+
+for i in n:
+    i += 1
+    print(f"i = {i}")
+    print("")
+    for j in n:
+        j = i
+        j += 1
+        print(f"j = {j}")
+    print("")
+    
